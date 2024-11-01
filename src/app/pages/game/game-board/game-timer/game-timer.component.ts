4b929d5633356ff17be2eec359ebdaf44d0046fa
StarPandaBeg <kshmurakov@mail.ru>
2024-11-01 11:14:28 +0300

    feat: add value getter

--- src/app/pages/game/game-board/game-timer/game-timer.component.ts
+++ src/app/pages/game/game-board/game-timer/game-timer.component.ts
@@ -6,6 +6,7 @@ import { GameBoardService } from '../game-board.service';
   selector: 'app-game-timer',
   standalone: true,
   imports: [DecimalPipe],
+  providers: [DecimalPipe],
   templateUrl: './game-timer.component.html',
 })
 export class GameTimerComponent implements OnDestroy {
@@ -14,12 +15,19 @@ export class GameTimerComponent implements OnDestroy {
 
   private timerInterval: ReturnType<typeof setInterval> | null = null;
 
-  constructor(private board: GameBoardService) {
+  constructor(
+    private board: GameBoardService,
+    private decimalPipe: DecimalPipe,
+  ) {
     effect(() => {
       const state = this.board.gameState();
       if (state === 'running') this.start();
       else this.stop();
     });
+  }
+
+  get value() {
+    return `${this.minutes}:${this.decimalPipe.transform(this.seconds, '2.0')}`;
   }
 
   ngOnDestroy(): void {
