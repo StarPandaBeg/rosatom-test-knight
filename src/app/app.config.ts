50e8fd1c521eca730c22fe59f2529a633062068b
StarPandaBeg <kshmurakov@mail.ru>
2024-11-01 11:55:50 +0300

    feat: add lucide icons

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,12 +2,20 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { LUCIDE_ICONS, LucideIconProvider, RefreshCcw } from 'lucide-angular';
 import { routes } from './app.routes';
+
+const usedIcons = { RefreshCcw };
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimations(),
+    {
+      provide: LUCIDE_ICONS,
+      multi: true,
+      useValue: new LucideIconProvider(usedIcons),
+    },
   ],
 };
